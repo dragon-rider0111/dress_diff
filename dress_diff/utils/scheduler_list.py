b15c0a14e2608bafb3d540cdfde0e6b851951764
dragon-rider0111 <haozeng0111@gmail.com>
2023-04-17 02:47:15 +0300

    update

--- dress_diff/utils/scheduler_list.py
+++ dress_diff/utils/scheduler_list.py
@@ -18,22 +18,22 @@ diff_scheduler_list = [
 
 
 def get_scheduler_list(pipe, scheduler):
-    if scheduler == scheduler_list[0]:
+    if scheduler == diff_scheduler_list[0]:
         pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
 
-    elif scheduler == scheduler_list[1]:
+    elif scheduler == diff_scheduler_list[1]:
         pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config)
 
-    elif scheduler == scheduler_list[2]:
+    elif scheduler == diff_scheduler_list[2]:
         pipe.scheduler = EulerDiscreteScheduler.from_config(pipe.scheduler.config)
 
-    elif scheduler == scheduler_list[3]:
+    elif scheduler == diff_scheduler_list[3]:
         pipe.scheduler = LMSDiscreteScheduler.from_config(pipe.scheduler.config)
 
-    elif scheduler == scheduler_list[4]:
+    elif scheduler == diff_scheduler_list[4]:
         pipe.scheduler = HeunDiscreteScheduler.from_config(pipe.scheduler.config)
 
-    elif scheduler == scheduler_list[5]:
+    elif scheduler == diff_scheduler_list[5]:
         pipe.scheduler = UniPCMultistepScheduler.from_config(pipe.scheduler.config)
 
     return pipe
