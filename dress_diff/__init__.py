831dfc70a4c7af410e4128f22a85977665925f69
Kadir Nar <kadir.nar@hotmail.com>
2023-04-16 14:15:41 +0300

    Add files via upload

---
+++ dress_diff/__init__.py
@@ -0,0 +1,33 @@
+# Controlnet Models
+from dress_diff.diffusion_models.controlnet import (
+    StableDiffusionControlNetCannyGenerator,
+    StableDiffusionControlNetDepthGenerator,
+    StableDiffusionControlNetHEDGenerator,
+    StableDiffusionControlNetMLSDGenerator,
+    StableDiffusionControlNetPoseGenerator,
+    StableDiffusionControlNetScribbleGenerator,
+    StableDiffusionControlNetSegGenerator,
+)
+
+# Diffusion Models
+from dress_diff.diffusion_models.stable_diffusion import (
+    StableDiffusionImage2ImageGenerator,
+    StableDiffusionText2ImageGenerator,
+)
+
+# Upscaler Models
+from dress_diff.upscaler_models import CodeformerUpscalerGenerator
+
+# Utils
+from dress_diff.utils import (
+    controlnet_canny_model_list,
+    controlnet_depth_model_list,
+    controlnet_hed_model_list,
+    controlnet_mlsd_model_list,
+    controlnet_pose_model_list,
+    controlnet_scribble_model_list,
+    controlnet_seg_model_list,
+    get_scheduler_list,
+    scheduler_list,
+    stable_model_list,
+)
